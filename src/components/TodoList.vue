645df9759a2f7315f0d97f9e8917a34a6faac3bb
oleg <olegskydonenko@gmail.com>
2020-03-29 14:56:53 +0300

    created component

---
+++ src/components/TodoList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      <span :class="{ 'done':todo.completed }"
+          @click="() => todo.completed = !todo.completed">
+        {{ todo.text }}
+      </span>
+      <button>X</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'one',
+          completed: false
+        },
+        {
+          id: 2,
+          text: 'two',
+          completed: true
+        },
+        {
+          id: 3,
+          text: 'three',
+          completed: false
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .done {
+    text-decoration: line-through;
+  }
+</style>
