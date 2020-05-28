9c706dac12615e40b9d36ae5099a5c4d96558d52
reboot-ua <reboot.if.ua@gmail.com>
2020-05-28 12:55:06 +0300

    Edit page corrected#3

--- src/components/EditProfilePage.vue
+++ src/components/EditProfilePage.vue
@@ -5,27 +5,27 @@
 
       <div class="form-group">
         <label>First Name</label>
-        <input type="text" v-model="profile.firstName" class="form-control form-control-lg"/>
+        <input type="text" v-model="user.firstName" class="form-control form-control-lg"/>
       </div>
 
       <div class="form-group">
         <label>Last Name</label>
-        <input type="text" v-model="profile.lastName" class="form-control form-control-lg"/>
+        <input type="text" v-model="user.lastName" class="form-control form-control-lg"/>
       </div>
 
       <div class="form-group">
         <label>Email address</label>
-        <input type="email" v-model="profile.email" class="form-control form-control-lg"/>
+        <input type="email" v-model="user.email" class="form-control form-control-lg"/>
       </div>
 
       <div class="form-group">
         <label>Phone Number</label>
-        <input type="email" v-model="profile.phone" class="form-control form-control-lg"/>
+        <input type="email" v-model="user.phone" class="form-control form-control-lg"/>
       </div>
 
       <div class="form-group">
         <label>Password</label>
-        <input type="password" v-model="profile.password" class="form-control form-control-lg"/>
+        <input type="password" v-model="user.password" class="form-control form-control-lg"/>
       </div>
 
       <button type="button" class="btn btn-dark btn-lg btn-block" v-on:click="save">Edit</button>
@@ -36,10 +36,10 @@
 
 <script>
 export default {
-  name: 'EditProfilePage',
+  name: 'EditProfile',
   data: function () {
     return {
-      profile: {
+      user: {
         firstName: '',
         lastName: '',
         email: '',
