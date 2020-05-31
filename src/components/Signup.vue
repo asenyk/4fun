5975d50fec79e8ebb3451f180bf7c70071c95d2b
reboot-ua <reboot.if.ua@gmail.com>
2020-05-31 21:25:47 +0300

    registration to DB

--- src/components/Signup.vue
+++ src/components/Signup.vue
@@ -5,30 +5,30 @@
 
       <div class="form-group">
         <label>First Name</label>
-        <input type="text" class="form-control form-control-lg"/>
+        <input type="text" class="form-control form-control-lg" v-model="firstName"/>
       </div>
 
       <div class="form-group">
         <label>Last Name</label>
-        <input type="text" class="form-control form-control-lg"/>
+        <input type="text" class="form-control form-control-lg" v-model="lastName"/>
       </div>
 
       <div class="form-group">
         <label>Email address</label>
-        <input type="email" class="form-control form-control-lg" />
+        <input type="email" class="form-control form-control-lg" v-model="emailAddress"/>
       </div>
 
       <div class="form-group">
         <label>Phone Number</label>
-        <input type="email" class="form-control form-control-lg" />
+        <input type="email" class="form-control form-control-lg" v-model="phoneNumber"/>
       </div>
 
       <div class="form-group">
         <label>Password</label>
-        <input type="password" class="form-control form-control-lg" />
+        <input type="password" class="form-control form-control-lg" v-model="password"/>
       </div>
 
-      <button type="submit" class="btn btn-dark btn-lg btn-block">Sign Up</button>
+      <button type="button" class="btn btn-dark btn-lg btn-block" @click="addData()">Sign Up</button>
 
       <p class="forgot-password text-right">
         Already registered
@@ -39,10 +39,34 @@
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'Signup',
   data () {
-    return {}
+    return {
+      firstName: '',
+      lastName: '',
+      emailAddress: '',
+      phoneNumber: '',
+      password: ''
+    }
+  },
+  methods: {
+    addData () {
+      const data = {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        emailAddress: this.emailAddress,
+        phoneNumber: this.phoneNumber,
+        password: this.password
+      }
+      console.log(data)
+      axios.post('users', data)
+        .then(response => {
+          console.log(response)
+        })
+    }
   }
 }
 </script>
