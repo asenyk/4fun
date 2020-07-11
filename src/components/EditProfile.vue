7d9188a7eb0ed6b8b88ac68a848778bb54ec9273
reboot-ua <reboot.if.ua@gmail.com>
2020-07-11 10:22:14 +0300

    validation edit page

--- src/components/EditProfile.vue
+++ src/components/EditProfile.vue
@@ -5,30 +5,76 @@
 
       <div class="form-group">
         <label>First Name</label>
-        <input type="text" v-model="user.firstName" class="form-control form-control-lg"/>
+        <input type="text"
+               class="form-control form-control-lg" :class="{ 'is-invalid': submitted && $v.user.firstName.$error }"
+               v-model="user.firstName"
+               @blur="$v.user.firstName.$touch()"
+        />
       </div>
+      <div v-if="$v.user.firstName.$error" class="error"> First Name is required</div>
 
       <div class="form-group">
         <label>Last Name</label>
-        <input type="tel" v-model="user.lastName" class="form-control form-control-lg"/>
+        <input type="text"
+               class="form-control form-control-lg" :class="{ 'is-invalid': submitted && $v.user.lastName.$error }"
+               v-model="user.lastName"
+               @blur="$v.user.lastName.$touch()"
+        />
       </div>
+      <div v-if="$v.user.lastName.$error" class="error"> Last Name is required</div>
 
       <div class="form-group">
         <label>Email address</label>
-        <input type="email" v-model="user.emailAddress" class="form-control form-control-lg"/>
+        <input
+          type="email"
+          class="form-control form-control-lg" :class="{ 'is-invalid': submitted && $v.user.emailAddress.$error }"
+          v-model="user.emailAddress"
+          @blur="$v.user.emailAddress.$touch()"
+        />
+      </div>
+      <div v-if="$v.user.emailAddress.$error" class="error">
+        <span v-if="!$v.user.emailAddress.required && $v.$touch()">Email is required</span>
+        <span v-else>Email is invalid</span>
       </div>
 
       <div class="form-group">
         <label>Phone Number</label>
-        <input type="tel" v-model="user.phoneNumber" class="form-control form-control-lg"/>
+        <input type="tel" class="form-control form-control-lg"
+               :class="{ 'is-invalid': submitted && $v.user.phoneNumber.$error }"
+               v-model="user.phoneNumber"
+               @blur="$v.user.phoneNumber.$touch()"
+        />
+      </div>
+      <div v-if="$v.user.phoneNumber.$error" class="error">
+        <span v-if="!$v.user.phoneNumber.required || $v.$touch()"> phone is required</span>
+        <span v-if="!$v.user.phoneNumber.numeric"> Its not a number</span>
+        <span
+          v-if="!$v.user.phoneNumber.maxLength || !$v.user.phoneNumber.minLength"> Number must have 10 numbers  </span>
+        <span v-else>phone is invalid</span>
       </div>
 
       <div class="form-group">
         <label>Password</label>
-        <input type="password" v-model="user.password" class="form-control form-control-lg"/>
+        <input type="password"
+               class="form-control form-control-lg" :class="{ 'is-invalid': submitted && $v.user.password.$error }"
+               v-model="user.password"/>
+      </div>
+      <div v-if="$v.user.password.$error" class="error">
+        <span v-if="!$v.user.password.required || $v.$touch()">Password is required</span>
+        <span v-if="!$v.user.password.minLength">Password must be at least 6 characters</span>
       </div>
 
-<p>{{$editData}}</p>
+      <div class="form-group">
+        <label>Confirm Password</label>
+        <input type="password"
+               class="form-control form-control-lg"
+               :class="{ 'is-invalid': submitted && $v.user.confirmPassword.$error }"
+               v-model="user.confirmPassword"/>
+      </div>
+      <div v-if="$v.user.confirmPassword.$error" class="error">
+        <span v-if="!$v.user.confirmPassword.required">Confirm Password is required</span>
+        <span v-else-if="!$v.user.confirmPassword.sameAsPassword">Passwords must match</span>
+      </div>
 
       <button
         type="button"
@@ -44,6 +90,7 @@
 <script>
 
 import axios from 'axios'
+import { email, required, numeric, maxLength, minLength, sameAs } from 'vuelidate/lib/validators'
 
 export default {
   name: 'EditProfile',
@@ -58,6 +105,17 @@ export default {
       }
     }
   },
+  validations: {
+    user: {
+      firstName: { required },
+      lastName: { required },
+      emailAddress: { required, email },
+      phoneNumber: { required, numeric, maxLength: maxLength(10), minLength: minLength(10) },
+      password: { required, minLength: minLength(6) },
+      confirmPassword: { required, sameAsPassword: sameAs('password') }
+
+    }
+  },
   methods: {
     editUser () {
       const editData = {
@@ -68,10 +126,9 @@ export default {
         password: this.user.password
       }
       const id = this.user.id
-      console.log(id)
-      axios.put('user/' + id, editData)
+      axios.put('users/' + id, editData)
         .then(response => {
-          this.$store.state.user = response.editData
+          this.$store.state.user = response.data
         })
     },
     save: function () {
